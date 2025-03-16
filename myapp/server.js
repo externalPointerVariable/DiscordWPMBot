81002a6a421bebdedc8acad5a376cd63652326da
externalPointerVariable <vicky986847@gmail.com>
2025-03-16 09:56:58 +0000

    Refactor server setup to spawn Discord bot process and improve logging

--- myapp/server.js
+++ myapp/server.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const path = require('path');
 const config = require('./config/config.js');
+const { spawn } = require('child_process');
 
-const app = express();
+const server = express();
 const port = config.port;
 
-app.use(express.static(path.join(__dirname, 'public')));
+server.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+server.get('/', (req, res) => {
   res.send('Discord Bot is Running 🔥');
 });
 
-app.get('/favicon.ico', (req, res) => {
+server.get('/favicon.ico', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'favicon.svg'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+const botProcess = spawn('node', ['src/index.js'], { stdio: 'inherit' });
+
+botProcess.on('exit', (code) => {
+  console.log(`Bot process exited with code ${code}`);
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
